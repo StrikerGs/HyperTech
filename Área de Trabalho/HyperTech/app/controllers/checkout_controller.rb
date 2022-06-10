b1c8e34a29e00dc8fc88338fbcecac2df6a30b51
StrikerGs <0123guga@gmail.com>
2022-06-10 18:08:43 -0300

    sessions

--- Área de Trabalho/HyperTech/app/controllers/checkout_controller.rb
+++ Área de Trabalho/HyperTech/app/controllers/checkout_controller.rb
@@ -20,6 +20,12 @@ class CheckoutController < ApplicationController
 
     def sucess
 
+      #session_with_expand = Stripe::Checkout::Session.retrieve({id: params[:session_id], expand: ["line_items"] })  
+      #session_with_expand.line_items.data.each do |line_item| 
+      #product = Product.find_by(stripe_product_id: line_item.price.product)
+      #product.increment!(:sales_count)
+      #end
+
 
 
     end
